000087c21d6d8797c40fba179320db2da73535fa
fatima-albaiti <fatima@faras.ae>
2023-01-31 14:48:56 +0400

    add content descriptions to images

--- app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
+++ app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
@@ -25,6 +25,16 @@ fun bindDetailsStatusImage(imageView: ImageView, isHazardous: Boolean) {
     }
 }
 
+@BindingAdapter("asteroidStatusImageDescription")
+fun bindImageContentDescription(imageView: ImageView, isHazardous: Boolean) {
+    if (isHazardous) {
+        imageView.contentDescription = imageView.context.getString(R.string.potentially_hazardous_asteroid_image)
+    }
+    else {
+        imageView.contentDescription = imageView.context.getString(R.string.not_hazardous_asteroid_image)
+    }
+}
+
 @BindingAdapter("astronomicalUnitText")
 fun bindTextViewToAstronomicalUnit(textView: TextView, number: Double) {
     val context = textView.context
